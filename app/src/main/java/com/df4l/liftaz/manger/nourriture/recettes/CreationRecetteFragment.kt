30182f0d5b0868ba40b94eb289f045c2b16759f2
df4l <defrenealexandre@gmail.com>
2025-11-20 20:36:36 +0100

    Corrige absence de TextWatcher sur QuantitePortion dans CreationRecetteFragment

--- app/src/main/java/com/df4l/liftaz/manger/nourriture/recettes/CreationRecetteFragment.kt
+++ app/src/main/java/com/df4l/liftaz/manger/nourriture/recettes/CreationRecetteFragment.kt
@@ -1,6 +1,8 @@
 package com.df4l.liftaz.manger.nourriture.recettes
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -84,6 +86,17 @@ class CreationRecetteFragment : Fragment() {
 
         recetteId = arguments?.getInt("recetteId")
         recetteId?.let { loadRecette(it) }
+
+        val editQuantitePortion = view.findViewById<TextInputEditText>(R.id.editQuantitePortion)
+        editQuantitePortion.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                // Recalcule les macros à chaque modification
+                updateTotalMacros()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
     }
 
     private fun loadRecette(id: Int) {
